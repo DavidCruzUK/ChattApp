a8e697ee277b99ed65e47511542d94803ca1729e
David Cruz Anaya <me@davidcruz.co.uk>
2020-11-26 17:58:14 +0000

    Create Chat area

--- app/src/main/java/com/lastreact/android/chattapp/ui/chat/ChatActivity.kt
+++ app/src/main/java/com/lastreact/android/chattapp/ui/chat/ChatActivity.kt
@@ -1,9 +1,16 @@
 package com.lastreact.android.chattapp.ui.chat
 
+import android.os.Bundle
+import android.view.View
 import com.lastreact.android.chattapp.base.BaseActivity
 import com.lastreact.android.chattapp.databinding.ActivityChatBinding
 
 class ChatActivity : BaseActivity<ActivityChatBinding>() {
+
     override fun createViewBinding(): ActivityChatBinding =
         ActivityChatBinding.inflate(layoutInflater)
+
+    fun sendClicked(view: View) {
+
+    }
 }
